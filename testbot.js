50664ae6b41eeb9db90a9aa3d7cf7323e1920b40
Joseph Nguyen <vietjew@gmail.com>
2015-07-21 00:39:27 -0700

    create sentence from spewer and tweet it

--- testbot.js
+++ testbot.js
@@ -1,9 +1,9 @@
 console.log("Your test bot is running!");
 var Twit = require('twit');
 var express = require('express');
+
 var spewer = require('./spewer.js');
-var sentence = spewer.spew('JJ JJ NNS VBP RB');
-console.log(sentence);
+
 var app = express();
 app.get('/', function (req, res) {
   res.send("Hello friends! See what we're up to at <a href='https://twitter.com/DrizzyAI'>Drizzy Drake's Twitter</a>.");
@@ -13,9 +13,18 @@ app.listen(port);
 
 var T = new Twit(require('./config.js'));
 
+
+
+function spewSentence() {
+  var sentence = spewer.spew('JJ JJ NNS VBP RB');
+  simpleTweet(sentence);
+}
+
 function simpleTweet(message) {
   T.post('statuses/update', {status: message},
   function(err, data, response) {
   console.log(data);
   });
 }
+
+spewSentence();
