cb0cc582474d34bd2a881dc1d07ecca5f21dc0fa
Joseph Nguyen <vietjew@gmail.com>
2015-07-21 00:34:33 -0700

    Spewer working with custom lexicon.

---
+++ spewer.js
@@ -0,0 +1,43 @@
+/*
+ * spewer
+ * https://github.com/dariusk/spewer
+ *
+ * Copyright (c) 2015 Darius Kazemi
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var _ = require('underscore');
+//
+// modified to use custom lexicon
+var lexicon = require('./drizzy_lexicon.json');
+
+function getWordByTag(tag) {
+  var words = _.chain(lexicon)
+    .map(function(v, k) {
+      if (_.indexOf(v, tag) > -1) {
+        return k;
+      }
+      else {
+        return null;
+      }
+    })
+    .filter(function(el) {
+      return el !== null;
+    })
+    .value();
+  return _.sample(words, 1);
+}
+
+module.exports = {
+  spew: function(tags) {
+    var out = '';
+    tags = tags.split(' ');
+    _.each(tags, function(tag) {
+      var word = getWordByTag(tag);
+      out += word + ' ';
+    });
+    return out.trim();
+  }
+};
--- testbot.js
+++ testbot.js
@@ -1,6 +1,9 @@
 console.log("Your test bot is running!");
 var Twit = require('twit');
 var express = require('express');
+var spewer = require('./spewer.js');
+var sentence = spewer.spew('JJ JJ NNS VBP RB');
+console.log(sentence);
 var app = express();
 app.get('/', function (req, res) {
   res.send("Hello friends! See what we're up to at <a href='https://twitter.com/DrizzyAI'>Drizzy Drake's Twitter</a>.");
